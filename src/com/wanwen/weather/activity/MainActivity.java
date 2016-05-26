f2d608be995838d56e927b2bc0fce2024e6f4081
zeng <419767374@qq.com>
2016-05-26 12:50:05 +0800

    First commit

---
+++ src/com/wanwen/weather/activity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.wanwen.weather.activity;
+
+import com.wanwen.weather.R;
+import com.wanwen.weather.R.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
